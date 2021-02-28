58b932e3d66c0a7db0eab16e9d4dd86b88d97a2f
AndreMarieKabeya <55158737+AndreMarieKabeya@users.noreply.github.com>
2021-02-28 12:55:30 -0500

    Added schema and seed for artists

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE artists (
+ name VARCHAR(20),
+ birthplace VARCHAR(20),
+ style VARCHAR(20),
+ dateofbirth DATE,
+ PRIMARY KEY (name)
+);
---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO artists
+(name, birthplace, style, dateofbirth, country)
+VALUES
+('Leonardo', 'Florence', 'Renaissance', '04-15-1452', 'Italy'),
+('Michelangelo', 'Arezzo', 'Renaissance', '03-06-1475',
+'Italy'),
+('Josefa', 'Seville', 'Baroque', '09-09-1630', 'Spain'),
+('Hans Hofmann', 'Weisenburg', 'Modern', '02-17-1966', 'Germany'),
+('John', 'San Francisco', 'Modern', '02-17-1920', 'USA');
