b4a1261550e5f64651a33efd3e6d1f74fd178452
AndreMarieKabeya <55158737+AndreMarieKabeya@users.noreply.github.com>
2021-03-28 13:37:23 -0400

    Update api.php

--- public/api.php
+++ public/api.php
@@ -1,4 +1,17 @@
 <?php
-$reply = ["hello" => "world"];
+//$reply = ["hello" => "world"];
+$all=getallheaders();
+$mutant=$all["X-Men"];
+
+switch ($mutant){
+	case "Wolverine":
+	$name="Logan";
+	break;
+	default:
+	$name="Unknown";
+}
+
+//header('Status: 400 Bad Request');
 header("Content-Type: application/json");
-echo json_encode($reply);
+$reply=["mutant" => $mutant,"name" => $name];
+echo json_encode($all);
